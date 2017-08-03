3a34db5c031529a354aacfbddd5f2a55ea4906ab
Tin Nguyen <Tinman@dhcp017102042040.corp.apple.com>
2017-08-03 18:55:32 -0500

    updating

--- models/index.js
+++ models/index.js
@@ -4,15 +4,14 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'production';
+var env       = process.env.JAWSDB_URL || process.env.NODE_ENV || 'production';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-// if(process.env.JAWSDB_URL){
-//   var sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } 
-// else 
-  if (config.use_env_variable) {
+if(process.env.JAWSDB_URL){
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
+} 
+else if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
